81abfff82e51855a791b677ec817e10986ae5e99
bshpetner00 <bshpetner00@stuy.edu>
2019-02-04 14:40:36 -0500

    made it

---
+++ Recursion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Recursion {
+  public static String f(int k, String s) {
+    return "hi";
+  }
+
+  public static List<String> makeAllWords(int k, int max) {
+    return null;
+  }
+
+}
